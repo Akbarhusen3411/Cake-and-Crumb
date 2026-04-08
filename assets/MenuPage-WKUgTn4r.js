e9abb5cc9f966a3ccfb92b8cf1aa0c7ccf40a6a1
Win10 <akbarhusain.momin2000@gmail.com>
2026-04-08 11:30:59 +0530

    Updates

---
+++ assets/MenuPage-WKUgTn4r.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DnA_Tdqi.js";import{F as t}from"./FeaturedCakes-D5ilcIzW.js";import"./cakes-BxzACL55.js";import"./useReviews-DBzfL6Am.js";import"./external-link-BVK1sVVv.js";function m(){return r.jsx("div",{className:"pt-20",children:r.jsx(t,{})})}export{m as default};
